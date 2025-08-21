d233adb8554bd0fa8e23491fd92e8d3dd0a7218d
Artemkkkka <temsha94@gmail.com>
2025-08-21 16:23:31 +0300

    Created global config

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
 
+from .config import settings
+from src.teams.router import teams_router
 
 app = FastAPI()
+app.include_router(teams_router)
+
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", reload=True)
+    uvicorn.run(
+        app="main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True
+    )
