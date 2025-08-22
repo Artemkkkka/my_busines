b7719dfce1eada479b359eb74222b2cff4e88ca6
Артём Брагин <bragin15bragin@yandex.ru>
2025-08-22 23:24:17 +0300

    Add get route for User

--- src/users/crud.py
+++ src/users/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .models import User
@@ -7,5 +8,12 @@ async def create_user(session: AsyncSession, email: str, password: str):
     user = User(email=email, password=password)
     session.add(user)
     await session.commit()
+
+    return user
+
+async def get_user(session: AsyncSession, user_id: int):
+    stmt = select(User).where(User.id == user_id)
+    result = await session.execute(stmt)
+    user = result.scalar_one()
 
     return user
--- src/users/router.py
+++ src/users/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import db_helper
-from .crud import create_user as crud_create_user
+from .crud import create_user as crud_create_user, get_user as crud_get_user
 from .schemas import UserRead, UserCreate
 
 
@@ -18,4 +18,12 @@ async def create_user(
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     user = await crud_create_user(session, email=payload.email, password=payload.password)
+    return user
+
+@users_router.get("/{user_id}", response_model=UserRead)
+async def get_user(
+    user_id: int,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    user = await crud_get_user(user_id=user_id, session=session)
     return user
